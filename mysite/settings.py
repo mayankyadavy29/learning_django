52500a911a52c69174a8312b353d13b7f46cd182
Mayank <mayankyadavy29@gmail.com>
2022-10-23 14:35:33 +0530

    Allowed all hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tl$(mt-!wd6m6n*l&rv+kz+owoo71u6-w#)%zd+(+$8e+$uwxu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
